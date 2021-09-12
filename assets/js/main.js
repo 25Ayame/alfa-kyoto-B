ad5cf375aaee35f08ed2f0deec457573c6730772
dansiJKrimaru <89884676+dansiJKrimaru@users.noreply.github.com>
2021-09-12 22:55:27 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'sansobonbe',
+          q1: 'SANSOBOBE',
         },
         stage2: {
           q1: 'いいい',
